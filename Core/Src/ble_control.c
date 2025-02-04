35b2870d3afc9f27848109ef98a722577a0a2136
Iskled Adebayo <111864415+iskled@users.noreply.github.com>
2025-02-04 13:31:52 +0000

    Update ble_control.c
    Implement Bluetooth control file

--- Core/Src/ble_control.c
+++ Core/Src/ble_control.c
@@ -1,8 +1,15 @@
-/*
- * ble_control.c
- *
- *  Created on: Feb 4, 2025
- *      Author: user
- */
+/* ble_control.c - Bluetooth Low Energy (BLE) Communication */
+#include "ble_control.h"
+#include "stm32l4xx_hal.h"
 
+extern UART_HandleTypeDef huart4;
 
+void BLE_Init(UART_HandleTypeDef *huart) {
+    // Initialize BLE module
+}
+
+void BLE_SendGesture(int gesture) {
+    char msg[20];
+    snprintf(msg, sizeof(msg), "Gesture: %d\r\n", gesture);
+    HAL_UART_Transmit(&huart4, (uint8_t*)msg, strlen(msg), HAL_MAX_DELAY);
+}
