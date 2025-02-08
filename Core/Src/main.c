a106e5c173e9b217894e879735ab35216b48fc56
Iskled Adebayo <111864415+iskled@users.noreply.github.com>
2025-02-08 23:57:37 +0000

    Update main.c
    Add documentation to the introductory part of the  main file

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -46,7 +46,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1; // I2C for MPU6050
-UART_HandleTypeDef huart4; // UART4 for BLE/Wi-Fi
+UART_HandleTypeDef huart4; // UART4 for BLE
 TIM_HandleTypeDef htim2; // Timer for periodic tasks
 
 /* USER CODE BEGIN PV */
@@ -84,7 +84,7 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -114,7 +114,7 @@ int main(void)
 
   /* USER CODE END 2 */
 
-  /* Infinite loop */
+
   /* USER CODE BEGIN WHILE */
   while (1)
   {
@@ -351,13 +351,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
+
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+
+
   __disable_irq();
   while (1)
   {
@@ -367,17 +366,12 @@ void Error_Handler(void)
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
+
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
